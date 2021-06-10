e498f009bed6dbb5f257aa12d78d5c9004117ae5
Lesan Alexandru <lesanalexandru14@gmail.com>
2021-06-11 00:40:04 +0300

    added comments

--- stripComments/script.js
+++ stripComments/script.js
@@ -1,18 +1,19 @@
-function solution(comment, markers) {
+function solution(text, markers) { // functia ce primeste textul și caracterele ce delimitează comentariile
 
-    var letters = comment.split('\n').map(el => el.split(''));
-    const regex = eval(`/[${markers.join('')}]/`);
+    var letters = text.split('\n').map(el => el.split('')); // desparțim textul în șiruri despartite de /n, iar fiecare șir îl despațim pe litere și plasam rezultatul într-o matrice
+    const regex = eval(`/[${markers.join('')}]/`); // creăm o expresie regulată cu caracterele ce delimitează comentariile
 
-    letters.forEach((el, pos) => {
-        el.forEach((letter, pos1) => {
-            if (regex.test(letter))
-                el.splice(pos1);
+    letters.forEach((el, pos) => {      // parcurgem textul pe siruri
+        el.forEach((letter, pos1) => {  // pargurgem șirurile pe caractere
+            if (regex.test(letter))     // verificam daca caracterul e inclus în expresia regulată
+                el.splice(pos1);        // în caz că da, ștergem toate caracterele șirului începand cu poziția pe care se află acesta
         })
-        letters[pos] = el.join('').trim();
+        letters[pos] = el.join('').trim(); // recreem șirul, concatinind elementele din matrice și ștergem spațiile de la inceput și sfarșit folosind metoda .trim()
     })
 
-    return letters.join('\n');
+    return letters.join('\n'); // returnam textul recreeat, adaugand '\n' între sirurile concatinate
 }
 
-console.log(solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"]));
+var result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"]); // efectuam testarea
+console.log(result); // afisam rezultatul
 
--- validateSudoku/script.js
+++ validateSudoku/script.js
@@ -1,53 +1,55 @@
-const positiveSudoku = require('./validTestCases'),
+const positiveSudoku = require('./validTestCases'), // importarea matricilor pentru efectuarea testelor
     negativeSudoku = require('./wrongTestCases');
 
-const sudokuValidator = (sudoku) => {
+const sudokuValidator = (sudoku) => {   // funcția în sine care primește matricea multi-dimensionala si returneaza rezultatul
 
-    const N = sudoku.length; // number of rows
-    const rootN = Math.sqrt(N);
+    const N = sudoku.length; // numarul de linii
+    const rootN = Math.sqrt(N); // radical din numarul de linii
     var validate = [], validate1 = [], correctRows = 0, correctColumns = 0; correctSquares = 0;
 
-    function validationRules() {
+    function validationRules() { // regulile de validate
         sudoku.forEach(row => validate.push(row.every(el => el >= 1 && el <= N && Number.isInteger(el)) && row.length === N));
+        // cu ajutorul metodelor ce opereaza cu matricile din js, parcurgem fiecare rand din matrice și inseram intr-o matrice unidimensionala rezultatul boolean true daca elementele sirului sunt nr intregi din intervalul 1...N și lungimea randului(numarul de coloane) este egal cu numarul de randuri
         return Number.isInteger(rootN) && rootN > 0 && validate.every(el => el === true);
+        // returnam true dacă rootN e intreg, > 0, și daca fiecare rand respecta conditiile de mai sus
     }
 
     if (validationRules()) {
         //! Rows and columns validation
-        for (let i = 0; i < N; i++) {
-            validate = Array(N).fill(0);
-            validate1 = Array(N).fill(0);
-            for (let j = 0; j < N; j++) { // elementul 
-                validate[sudoku[i][j] - 1]++; // for rows
-                validate1[sudoku[j][i] - 1]++; // for columns
+        for (let i = 0; i < N; i++) {               // parcurgeri
+            validate = Array(N).fill(0);     // initializam vectori de lungime N si-i completam cu 0
+            validate1 = Array(N).fill(0);    // contor pt numarul de apariții a fiecărei cifre, pozitia insemnand cifra in sine
+            for (let j = 0; j < N; j++) {
+                validate[sudoku[i][j] - 1]++;       // for rows => incrementam numarul de aparitii a cifrei pe linii
+                validate1[sudoku[j][i] - 1]++;      // for columns => scădem 1 la pozitie pt că initializarea incepe de la 0
             }
-            correctRows += validate.every(el => el === 1);
+            correctRows += validate.every(el => el === 1); // incrementam numarul de randuri corecte daca numarul de aparitii a fiecărei cifre e exact 1
             correctColumns += validate1.every(el => el === 1);
         }
 
-        if (correctColumns === N && correctRows === N) {
-
+        if (correctColumns === N && correctRows === N) { // daca numarul de randuri și coloane corecte este cel necesar, putem trece la verificarea patratelor din interior
+            // am adaugat conditia pentru a nu face o parcurgere suplimentara in cazul in care soluția sudoku e greșita
             //! littleSquares validation
             var line = 0, column = 0;
             while (line <= N - rootN) {
                 while (column <= N - rootN) {
                     validate = Array(N).fill(0);
                     for (let i = line; i < line + rootN; i++)
-                        for (let j = column; j < column + rootN; j++)
-                            validate[sudoku[i][j] - 1]++;
-                    correctSquares += validate.every(el => el === 1);
+                        for (let j = column; j < column + rootN; j++) // parcurgem matricea astfel incat sa putem parcurge pe rand patratele mici
+                            validate[sudoku[i][j] - 1]++; // numarul de aparitii a cifrei in patrat
+                    correctSquares += validate.every(el => el === 1); // numarul patratelor corecte
                     column += rootN;
                 } line += rootN; column = 0;
             }
 
-            return correctSquares === N ? 'Sudoku has been filled out CORRECTLY'
+            return correctSquares === N ? 'Sudoku has been filled out CORRECTLY' // folosind operatorul ternar returnam rezultatul final
                 : `${N - correctSquares} Little squares (${rootN}X${rootN}) has been filled out WRONG`;
-        } else return N - correctRows ? `${N - correctRows} rows has been filled out WRONG`
+        } else return N - correctRows ? `${N - correctRows} rows has been filled out WRONG` // pt a sti exact cate randuri sau coloane sunt completate gresit
             : N - correctColumns ? `${N - correctColumns} columns has been filled out WRONG` : 'error';
-    } else return 'Validation rules are not followed';
+    } else return 'Validation rules are not followed';  // mesajul returnat in cazult in care regulile de validare nu sunt respectate
 }
 
-console.log('-------------- Positive Test Cases --------------');
-positiveSudoku.forEach(sudokuData => console.log(sudokuValidator(sudokuData)));
+console.log('-------------- Positive Test Cases --------------');   // efectuarea testărilor
+positiveSudoku.forEach((sudokuData, index) => console.log('Test ' + (index + 1) + ': ' + sudokuValidator(sudokuData)));
 console.log('-------------- Negative Test Cases --------------');
-negativeSudoku.forEach(sudokuData => console.log(sudokuValidator(sudokuData)));
+negativeSudoku.forEach((sudokuData, index) => console.log('Test ' + (index + 1) + ': ' + sudokuValidator(sudokuData)));
